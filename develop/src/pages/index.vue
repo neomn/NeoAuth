4cdbcd892586e020c09ce53861e5ae9ea9497531
neomn110 <neomn110@gmail.com>
2023-12-11 15:36:45 +0330

    feat: read qrcode successfully

--- develop/src/pages/index.vue
+++ develop/src/pages/index.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="flex h-screen bg-gradient-to-br from-gray-600 to-gray-900">
     <VitePwaManifest/>
-    <qrcode-stream class="border"></qrcode-stream>
+    <qrcode-stream @detect="onQrDetect" class="border"></qrcode-stream>
     <qrcode-drop-zone class="border border-lime-300"></qrcode-drop-zone>
     <qrcode-capture class="border border-yellow-400"></qrcode-capture>
   </div>
@@ -19,6 +19,11 @@ export default {
     QrcodeStream,
     QrcodeDropZone,
     QrcodeCapture
+  },
+  methods: {
+    onQrDetect(detectedCodes){
+      console.log(detectedCodes) 
+    },
   },
 }
 </script>
