034218e680c80d41780d6bd57b4dc12439e2b009
neomn110 <neomn110@gmail.com>
2023-12-11 10:43:23 +0330

    feat: setup pwa

--- develop/src/nuxt.config.ts
+++ develop/src/nuxt.config.ts
@@ -2,15 +2,48 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss',
+    '@nuxtjs/tailwindcss', 
+    '@vite-pwa/nuxt',
   ],
-  buildModules: ['@nuxtjs/pwa'],
   pwa: {
     manifest: {
       name: 'name',
       description: "Desc",
       theme_color: '#607D8B',
-      short_name: 'shortname'
+      short_name: 'shortname',
+      icons: [
+        {
+          "src": "icons/manifest-icon-192.maskable.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "any"
+        },
+        {
+          "src": "icons/manifest-icon-192.maskable.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable"
+        },
+        {
+          "src": "icons/manifest-icon-512.maskable.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "any"
+        },
+        {
+          "src": "icons/manifest-icon-512.maskable.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ],
+    },
+    workbox: {
+      navigateFallback: "/",
+    },
+    devOptions: {
+      enabled: true,
+      type: "module",
     },
   },
 })
