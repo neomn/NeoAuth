c465d9b5ea265fe816f20c0fcd06e36cc00aa759
neomn110 <neomn110@gmail.com>
2023-12-13 17:46:18 +0330

    feat: create permissions helper

---
+++ develop/src/helpers/Permissions.js
@@ -0,0 +1,19 @@
+var grant_camera_permission = ()=>{
+  
+}
+var grant_file_permission = ()=>{
+  
+}
+var grant_clipboard_permission = ()=>{
+  
+}
+
+var has_camera_permission = ()=>{
+  
+}
+var has_file_permission = ()=>{
+  
+}
+var has_clipboard_permission = ()=>{
+  
+}
