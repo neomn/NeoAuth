88f10d6fb804c5c02f7a4e145303f5a552de8f7e
neomn110 <neomn110@gmail.com>
2023-12-13 17:47:28 +0330

    feat: create startup operations

---
+++ develop/src/operations/startup.js
@@ -0,0 +1,14 @@
+//operations which runs every time that app runs
+
+export function startup_ops(router){
+  if (false){
+    console.log('this is initial run ')
+    router.push('welcome')
+  } else {
+     
+  }
+}
+
+var is_initial_run = ()=>{return false}
+
+
