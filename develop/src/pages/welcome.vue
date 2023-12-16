f0e160d4618efcea9e2b1ac65e8d701bb5be372e
neomn110 <neomn110@gmail.com>
2023-12-16 19:00:36 +0330

    refactor: run welcome operations on component mount

--- develop/src/pages/welcome.vue
+++ develop/src/pages/welcome.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
+  <div class="h-screen bg-gradient-to-br from-gray-600 to-gray-800">
     this is welcome
+    <button @click="grantRequiredPermissions" class="border">grant permissions</button>
   </div>
 </template>
 
 
 
 <script>
-  
-export default { 
-
-}
-
+  import {welcomeOperations} from '../operations/welcome.js'
+  export default { 
+    mounted() {
+      welcomeOperations()
+    },
+  }
 </script>
