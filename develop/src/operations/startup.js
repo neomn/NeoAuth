44da2150fceaa94e1429dc03817db33b867e0342
neomn110 <neomn110@gmail.com>
2023-12-16 18:58:51 +0330

    refactor: remove unnecesarry code lines

--- develop/src/operations/startup.js
+++ develop/src/operations/startup.js
@@ -1,14 +1,10 @@
 //operations which runs every time that app runs
 
 export function startup_ops(router){
-  if (is_initial_run){
-    console.log('this is initial run ')
+  if (is_initial_run())
     router.push('welcome')
-  } else {
-     
-  }
 }
 
-var is_initial_run = ()=>{return true}
+var is_initial_run = ()=> localStorage.getItem('storage_path') === null
 
 
