9921e668979767f0caa25ebaabe461c23ca9d69d
neomn110 <neomn110@gmail.com>
2023-12-20 17:20:27 +0330

    refactor: add getAllPermissions template'

--- develop/src/helpers/Permissions.js
+++ develop/src/helpers/Permissions.js
@@ -6,4 +6,21 @@ const REQUIRED_PERMISSIONS = [
 
 export default {
   
+  grantAllPermissions(){
+    // for p in permissions:
+      // if p not granted yet:
+        // if status not granted:
+          // return false
+    
+    //return true
+
+    // REQUIRED_PERMISSIONS.forEach(element => {
+    //   console.log('granting ', element, " permission")
+    // });
+    // return this.getNotGrantedPermissions() ? false : true;
+  },
+  
+  getNotGrantedPermissions(){
+    return false
+  },
 }
