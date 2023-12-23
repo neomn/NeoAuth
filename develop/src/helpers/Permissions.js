0d30b1a4395791895f597cb70a2dabaa63fda190
neomn110 <neomn110@gmail.com>
2023-12-23 16:06:11 +0330

    feat: grant file system permission

--- develop/src/helpers/Permissions.js
+++ develop/src/helpers/Permissions.js
@@ -16,6 +16,18 @@ export default {
     })
     console.log('clipboard:',clipboard)
 
+
+    // grant file access permission if already not granted
+    if ('showOpenFilePicker' in window) 
+      file = true
+    else {
+      await window.showOpenFilePicker()
+      if ('showOpenFilePicker' in window) 
+        file = true
+    }
+    console.log('file:', file)
+
+
     
   }
 }
