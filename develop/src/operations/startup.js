9ee0b3cc97a91450cf0c87935b6e53c4da89b887
neomn110 <neomn110@gmail.com>
2023-12-23 15:48:52 +0330

    style: reformat

--- develop/src/operations/startup.js
+++ develop/src/operations/startup.js
@@ -1,10 +1,12 @@
 //operations which runs every time that app runs
 
+
+var is_initial_run = ()=> localStorage.getItem('storage_path') === null
+
 export function startup_ops(router){
   if (is_initial_run())
     router.push('welcome')
 }
 
-var is_initial_run = ()=> localStorage.getItem('storage_path') === null
 
 
