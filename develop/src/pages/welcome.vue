368aff0260a5ccfbdbb0f27a882a3810f75f159d
neomn110 <neomn110@gmail.com>
2023-12-24 16:00:48 +0330

    refactor: change buttons texts

--- develop/src/pages/welcome.vue
+++ develop/src/pages/welcome.vue
@@ -1,8 +1,12 @@
 <template>
-    <div class="h-screen flex flex-col justify-around items-center bg-gradient-to-br from-gray-600 to-gray-800">
+  <div class="h-screen flex flex-col justify-around items-center bg-gradient-to-br from-gray-600 to-gray-800">
     
 
+    <div class='flex flex-col m-4 '>
+      <Button  @click="setupStoragePath" text='Setup Storage Path' class='text-gray-300 m-2'/>
+      <Button v-if="!first_atempt" @click="exit" text='Load Files From Previous Installation' class='text-gray-300 m-2' />
     </div>
+  </div>
 </template>
 
 
