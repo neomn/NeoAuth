5f6b91b937eacc33d947aab50d5f1d5371049d6a
neomn110 <neomn110@gmail.com>
2023-12-24 15:56:29 +0330

    refactor: rename storage_path to path

--- develop/src/operations/startup.js
+++ develop/src/operations/startup.js
@@ -1,7 +1,7 @@
 //operations which runs every time that app runs
 
 
-var is_initial_run = ()=> localStorage.getItem('storage_path') === null
+var is_initial_run = ()=> localStorage.getItem('path') === null
 
 export function startup_ops(router){
   if (is_initial_run())
