5bff7abc994770a17969d115c927483c9ff9ad78
neomn110 <neomn110@gmail.com>
2023-12-24 07:33:33 +0330

    style: reformat

--- develop/src/helpers/Permissions.js
+++ develop/src/helpers/Permissions.js
@@ -1,50 +1,37 @@
-function deviceHasCamera() {
-  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-}
 
 export default {
   
-  async grantRequiredPermissions(){
-    let camera = false, clipboard = false, file = false
-    
+    async grantRequiredPermissions(){
+        let camera = false, clipboard = false, file = false
 
-    // grant clipboard permission if already not granted
-    await navigator.permissions.query({name: 'clipboard-read'}).then(result => {  
-      if (result.state == 'granted')
-        clipboard = true
-      else navigator.clipboard.readText().then(() => { clipboard = true}).catch(err=>{console.log(err)})
-    }).catch(error=>{console.log(error)})
-
-
-    // grant file access permission if already not granted
-    if ('showOpenFilePicker' in window) 
-      file = true
-    else {
-      await window.showOpenFilePicker()
-      if ('showOpenFilePicker' in window) 
-        file = true
-    }
-    console.log('file:', file)
+        // grant clipboard permission if already not granted
+        await navigator.permissions.query({name: 'clipboard-read'}).then(result => {  
+            if (result.state == 'granted')
+                clipboard = true
+            else navigator.clipboard.readText().then(() => { clipboard = true}).catch(err=>{console.log(err)})
+                }).catch(error=>{console.log(error)})
 
 
-    // grant camera permission if device has camera and already not granted
-    if (deviceHasCamera()){
-      await navigator.permissions.query({name:'camera'}).then(result => {
-        if (result.state === 'granted') 
-          camera = true
-        else  navigator.mediaDevices.getUserMedia({video: true}).then(result=>{
-          if (result.state === 'granted')
-            camera = true
-        }).catch(err=>{console.log(err)})
-      }).catch(err=>{console.log(err)})
-    }
-    
-    console.log('device has camera:',deviceHasCamera())
-    console.log('camera:',camera)
-    console.log('clipboard:', clipboard)
-    console.log('file:', file)
-    
-    return ((deviceHasCamera && camera && file && clipboard) || (!deviceHasCamera && file && clipboard))
+        // grant file access permission if already not granted
+        if ('showOpenFilePicker' in window) 
+            file = true
+        else {
+            await window.showOpenFilePicker()
+            if ('showOpenFilePicker' in window) 
+                file = true
+        }
+        console.log('file:', file)
 
+
+        // grant camera permission if device has camera and already not granted
+        if (true){
+            await navigator.mediaDevices.getUserMedia({video: true}).then(result=>{
+                console.log('get camera permission: ', result)
+            })
+        }
+        
+        console.log('camera:',camera)
+        console.log('clipboard:', clipboard)
+        console.log('file:', file)
   }
 }
